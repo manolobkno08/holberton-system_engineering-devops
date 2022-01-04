b0bfedee382c3222fde9116ae600e64fe04b9f7a
manolobkno08 <manologm10@hotmail.com>
2022-01-04 17:43:51 -0500

    task4 fixed

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,14 @@
 # Puppet
 
 file_line { 'config':
-  path => '/etc/ssh/ssh_config',
-  line => '	IdentityFile ~/.ssh/school',
+  path    => '/etc/ssh/ssh_config',
+  line    => '	IdentityFile ~/.ssh/school',
   replace => 'true',
 }
 
 file_line { 'config2':
-4   path => '/etc/ssh/ssh_config',
-5   line => '     PasswordAuthentication no',
+4   path    => '/etc/ssh/ssh_config',
+5   line    => '     PasswordAuthentication no',
 6   replace => 'true',
 7 }
 
