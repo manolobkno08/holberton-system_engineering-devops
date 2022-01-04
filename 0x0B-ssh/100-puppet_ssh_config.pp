55dbc33ffcd1339dfe2e6423d5fcb8d33fb5738d
manolobkno08 <manologm10@hotmail.com>
2022-01-04 17:57:19 -0500

    task4 fixed

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -5,12 +5,12 @@ include stdlib
 file_line { 'file':
   path    => '/etc/ssh/ssh_config',
   line    => '	IdentityFile ~/.ssh/school',
-  replace => 'true',
+  replace => true,
 }
 
 file_line { 'skip password':
 4   path    => '/etc/ssh/ssh_config',
 5   line    => '     PasswordAuthentication no',
-6   replace => 'true',
+6   replace => true,
 7 }
 
