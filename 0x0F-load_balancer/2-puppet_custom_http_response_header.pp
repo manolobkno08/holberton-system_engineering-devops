79d84aab3291067a414302f4c2eb10fd27dea0a5
manolobkno08 <manologm10@hotmail.com>
2022-01-10 23:58:59 -0500

    add task2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -3,28 +3,28 @@
 $hname="add_header X-Served-By ${hostname};";
 
 exec { 'update' :
-	command => '/user/bin/apt-get update',
+  command => '/user/bin/apt-get update',
 }
 -> package { 'nginx' :
-	ensure => installed,
-	require => Exec['update'],
+  ensure  => installed,
+  require => Exec['update'],
 }
 -> file_line { 'redirection' :
-	ensure => present,
-	path => '/etc/nginx/sites-available/default',
-	after => 'listen 80 defatul_server;',
-	line => 'rewrite ^/redirect_me https://www.linkedin.com/in/manuel-alejandro-gomez-883951120/ permanent;',
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 defatul_server;',
+  line   => 'rewrite ^/redirect_me https://www.linkedin.com/in/manuel-alejandro-gomez-883951120/ permanent;',
 }
 -> file_line { 'custom http server' :
-	ensure => present,
-	path => '/etc/nginx/sites-available/default',
-	after => 'listen 80 defatul_server;',
-	line => $hname,
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 defatul_server;',
+  line   => $hname,
 }
 -> file { '/var/www/html/index.html' :
-	content => 'Hello World',
+  content => 'Hello World',
 }
 -> services { 'nginx' :
-	ensure => 'running',
-	require => Package['nginx'],
+  ensure  => 'running',
+  require => Package['nginx'],
 }
