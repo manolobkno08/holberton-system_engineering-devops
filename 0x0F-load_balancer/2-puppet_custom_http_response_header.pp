43fb5f71a004e9c5884f7ccd2ef693911e2cade7
manolobkno08 <manologm10@hotmail.com>
2022-01-10 23:56:37 -0500

    add task2

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,30 @@
+# Add a custom HTTP header with Puppet
+
+$hname="add_header X-Served-By ${hostname};";
+
+exec { 'update' :
+	command => '/user/bin/apt-get update',
+}
+-> package { 'nginx' :
+	ensure => installed,
+	require => Exec['update'],
+}
+-> file_line { 'redirection' :
+	ensure => present,
+	path => '/etc/nginx/sites-available/default',
+	after => 'listen 80 defatul_server;',
+	line => 'rewrite ^/redirect_me https://www.linkedin.com/in/manuel-alejandro-gomez-883951120/ permanent;',
+}
+-> file_line { 'custom http server' :
+	ensure => present,
+	path => '/etc/nginx/sites-available/default',
+	after => 'listen 80 defatul_server;',
+	line => $hname,
+}
+-> file { '/var/www/html/index.html' :
+	content => 'Hello World',
+}
+-> services { 'nginx' :
+	ensure => 'running',
+	require => Package['nginx'],
+}
