d62836c5f062573814706daf54dd68c38f1e7cfd
manolobkno08 <manologm10@hotmail.com>
2022-01-31 18:43:51 -0500

    add task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""
+START REST API SERVICE
+"""
+import csv
+import requests as reqs
+from sys import argv
+
+
+def run():
+    """ Return users by tasks completed successfully"""
+    u_id = argv[1]
+
+    url = 'https://jsonplaceholder.typicode.com/'
+    tasks = reqs.get(url + 'todos?userId={}'.format(u_id)).json()
+    user = reqs.get(url + 'users/{}'.format(u_id)).json()
+
+    csv_inf = []
+    for iter in tasks:
+        csv_inf.append([u_id,
+                        user.get('username'),
+                        iter.get('completed'),
+                        iter.get('title')])
+
+    filename = u_id + '.csv'
+    with open(filename, 'w', newline='') as csv_file:
+        r = csv.writer(csv_file, delimiter=',', quoting=csv.QUOTE_ALL)
+        r.writerows(csv_inf)
+
+
+if __name__ == "__main__":
+    run()
