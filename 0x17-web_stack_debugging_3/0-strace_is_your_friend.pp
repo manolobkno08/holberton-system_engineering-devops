362e18541f883fb408eced91a9405e83e2bc0205
manolobkno08 <manologm10@hotmail.com>
2022-02-16 15:12:49 -0500

    ADD: Config bug fixed

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+include stdlib
+file_line { 'debug':
+	ensure  => present,
+	path	=> 'var/www/html/wp-config.php',
+	line	=> 'require_once( ABSPATH . WPINC . '/class-wp-locale.php' );',
+	match => 'require_once( ABSPATH . WPINC . '/class-wp-locale.phpp' );',
+}
