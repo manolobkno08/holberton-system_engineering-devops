460ba5affa97c8a2ef5be31481a76f73c67825ac
manolobkno08 <manologm10@hotmail.com>
2022-02-15 19:59:05 -0500

    ADD: fetch number of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from cmath import e
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Get reddit subscribers"""
+    try:
+        url = 'https://www.reddit.com/r/{subreddit}/about.json'.format(
+            subreddit=subreddit)
+        request = requests.get(
+            url, headers={'User-agent': 'MyHolbertonAPI/0.0.1'})
+        r = request.json()['data']['subscribers']
+        return r
+    except(Exception):
+        return(0)
