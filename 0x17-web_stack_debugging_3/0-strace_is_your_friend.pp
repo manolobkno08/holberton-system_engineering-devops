d825f1b463afe0b3a33c65d3b03cabfddaf4f404
manolobkno08 <manologm10@hotmail.com>
2022-02-16 19:19:55 -0500

    UPD: / before to path

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,7 +2,7 @@
 include stdlib
 file_line { 'debug':
   ensure => present,
-  path   => 'var/www/html/wp-settings.php',
+  path   => '/var/www/html/wp-settings.php',
   line   => 'require_once( ABSPATH . WPINC . '/class-wp-locale.php' );',
   match  => 'require_once( ABSPATH . WPINC . '/class-wp-locale.phpp' );',
 }
